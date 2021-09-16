cd95dcab87f0b1e2cf181b61c45d48e5351ff511
quiptobasco <quiptobasco@gmail.com>
2021-09-15 20:14:44 -0700

    updating javascript

--- assets/js/tim.scripts.js
+++ assets/js/tim.scripts.js
@@ -1,10 +1,11 @@
 var key = "903e9e034afc51efaa5991d33e9e4306";
 
-var generatedLyrics = [];
+var generatedResults = [];
 
 function artistResults(json) {
     if (json.message.header.status_code == "200" && json.message.body.artist.artist_name !== "") {
         artist = json.message.body.artist.artist_name;
+        console.log("Artist name: " + artist);
         getTracks(artist);
     } else {
         console.log("artistsResults() had an error.  Starting over.");
@@ -13,21 +14,25 @@ function artistResults(json) {
 }
 
 function trackResults(json) {
+    generatedResults = [];
+    console.log(json);
+    
     if (json.message.header.status_code == "200" && json.message.body.track_list.length > 0) {
-        var goodSongs = [];
         for (var i = 0; i < json.message.body.track_list.length; i++) {
+            console.log(json.message.body.track_list[i].track.track_id);
             if (json.message.body.track_list[i].track.explicit == 0 && json.message.body.track_list[i].track.has_lyrics == 1) {
-                goodSongs.push( {"trackID": json.message.body.track_list[i].track.track_id, 
+                generatedResults.push( {"trackID": json.message.body.track_list[i].track.track_id, 
                 "artist": json.message.body.track_list[i].track.artist_name, 
                 "trackName": json.message.body.track_list[i].track.track_name });
             }
         }
-        if (goodSongs.length == 0) {
-            console.log(goodSongs.length);
+        if (generatedResults.length == 0 || generatedResults.length < 5) {
             getArtist();
         } else {
-            var track = randomize(goodSongs.length);
-            var trackID = goodSongs[track];
+            var answer = randomize(generatedResults.length);
+            var trackID = generatedResults[answer];
+            generatedResults.push( { "correctID": trackID });
+            generatedResults.splice([answer], 1);
             getLyrics(trackID);
         }
     } else {
@@ -38,7 +43,8 @@ function trackResults(json) {
 
 function lyricResults(json) {
     if (json.message.header.status_code == "200" && json.message.body.lyrics_body !== "") {
-        console.log(json.message.body.lyrics.lyrics_body);
+        lyric = json.message.body.lyrics.lyrics_body;   
+        generateQuestions(lyric);
     } else {
         console.log("lyricResults() had an error.  Starting over.");
         getArtist();
@@ -62,13 +68,43 @@ function getLyrics(trackID) {
     $.ajax({ url: trackURL, dataType: "jsonp", jsonpCallback: "lyricResults" });
 }
 
+function generateQuestions(lyric) {
+    var question = [];
+    var answer = generatedResults[generatedResults.length - 1].correctID.trackName;
+    console.log(answer);
+    // { lyric: lyric,
+    // trackname1: trackname1,
+    // trackname2: trackname2,
+    // trackname3: trackname3,
+    // trackname4: trackname4,
+    // correctAnswer: selectedtrackname }
+    console.log(generatedResults);
+    question.push( { "lyric": lyric } );
+    for (var i = 0; i < 5; i++) {
+        var randomIndex = randomize(generatedResults.length);
+        var random = generatedResults[randomIndex].trackName;
+        console.log(random);
+        question.push( { "trackname": generatedResults[randomIndex].trackName} );
+        generatedResults.splice([randomIndex], 1);
+    }
+    var randomTrack = randomize(question.length);
+    question[randomTrack].trackname = answer;
+    console.log(question);
+    question.push( { "correctAnswer": answer} );
+}
+
 
 function randomize(length) {
     return Math.floor(Math.random() * (length - 1));
 }
 
+function init() {
+    generatedResults = [];
+}
 
-$(".button-search").click(function(event) {
+init();
+
+$(".button-random").click(function(event) {
     event.preventDefault();
     getArtist();
 });
