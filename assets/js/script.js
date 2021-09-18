04a87bb496a49c47f134a6e2f6d751f6c1a6f181
quiptobasco <quiptobasco@gmail.com>
2021-09-18 13:39:14 -0700

    updated the question function to not have as many duplicate results

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,99 +11,109 @@ var key = "903e9e034afc51efaa5991d33e9e4306";
 var generatedResults = [];
 var answer = "";
 
+// json results for artist search
 function artistResults(json) {
-    if (json.message.header.status_code == "200" && json.message.body.artist.artist_name !== "") {
-        artist = json.message.body.artist.artist_name;
-        getTracks(artist);
-    } else {
-        console.log("artistsResults() had an error.  Starting over.");
-        getArtist();
-    }
+  //
+  if (json.message.header.status_code == "200" && json.message.body.artist.artist_name !== "") {
+    artist = json.message.body.artist.artist_name;
+    getTracks(artist);
+  } else {
+    console.log("artistsResults() had an error.  Starting over.");
+    getArtist();
+  }
 }
 
+// json results for track array from searched artist
 function trackResults(json) {
-    results = [];
-    generatedResults= [];
-    
-    if (json.message.header.status_code == "200" && json.message.body.track_list.length > 0) {
-      console.log(json);
-        for (var i = 0; i < 5; i++) {
-            if (json.message.body.track_list[i].track.explicit == 0 && json.message.body.track_list[i].track.has_lyrics == 1) {
-              if (!results.includes(json.message.body.track_list[i].track.track_name)) {
-                generatedResults.push( {"trackID": json.message.body.track_list[i].track.track_id, 
-                "artist": json.message.body.track_list[i].track.artist_name, 
-                "trackName": json.message.body.track_list[i].track.track_name });
-            }
-          }
+
+  //empty array to store generated track list
+  generatedResults= [];
+    //check to make sure we get a result, and that there's at least 5 tracks
+    if (json.message.header.status_code == "200" && json.message.body.track_list.length > 5) {
+      for (var i = 0; i < 5; i++) {
+        //check to make sure track isn't explicit, and has lyrics and add it to the array of results
+        if (json.message.body.track_list[i].track.explicit == 0 && json.message.body.track_list[i].track.has_lyrics == 1)
+        generatedResults.push( {"trackID": json.message.body.track_list[i].track.track_id, 
+                                "artist": json.message.body.track_list[i].track.artist_name, 
+                                "trackName": json.message.body.track_list[i].track.track_name });
         }
+        //if no or too few results meet our criteria pull another random artist
         if (generatedResults.length == 0 || generatedResults.length < 5) {
-            getArtist();
+          getArtist();
         } else {
-            var random = randomize(generatedResults.length);
-            var trackID = generatedResults[random];
-            answer = trackID.trackName;
-            console.log(generatedResults);
-            getLyrics(trackID);
+          //otherwise pick a track at random and get the lyrics, and set it as the answer
+          var random = randomize(generatedResults.length);
+          var trackID = generatedResults[random];
+          answer = trackID.trackName;
+          console.log(generatedResults);
+          getLyrics(trackID);
         }
     } else {
-        console.log("trackResults() had an error.  Starting over.");
-        getArtist();
+      //no good results, pull another random artist
+      console.log("trackResults() had an error.  Starting over.");
+      getArtist();
     }
 }
 
+// json results for lyrics to specific track
 function lyricResults(json) {
-    if (json.message.header.status_code == "200" && json.message.body.lyrics_body !== "") {
-        var lyric = json.message.body.lyrics.lyrics_body.split(/\r?\n/);
-        var lyrics = [];
-        for (var i = 0; i < 3; i++) {
-          lyrics += lyric[i] + "\n";
-        }  
-        generateQuestions(lyrics);
-    } else {
-        console.log("lyricResults() had an error.  Starting over.");
-        getArtist();
-    }
+  if (json.message.header.status_code == "200" && json.message.body.lyrics_body !== "") {
+    //split lyrics at \n (new line) to make it only display 2 lines
+    var lyric = json.message.body.lyrics.lyrics_body.split(/\r?\n/);
+    var lyrics = [];
+    for (var i = 0; i < 3; i++) {
+      lyrics += lyric[i] + "\n";
+    }  
+    //generate the questions and send it with the lyrics
+    generateQuestions(lyrics);
+  } else {
+    //no good lyrics, call a new random artist 
+    console.log("lyricResults() had an error.  Starting over.");
+    getArtist();
+  }
 }
 
+//call to the API to find an artist at random, returns as a callback function
 function getArtist() {
-    var randomID = randomize(9999);
-    var artistURL = "https://api.musixmatch.com/ws/1.1/artist.get?format=jsonp&callback=callback&artist_id=" + randomID + "&apikey=" + key;
-    $.ajax({ url: artistURL, dataType: "jsonp", jsonpCallback: "artistResults" });
+  var randomID = randomize(9999);
+  var artistURL = "https://api.musixmatch.com/ws/1.1/artist.get?format=jsonp&callback=callback&artist_id=" + randomID + "&apikey=" + key;
+  $.ajax({ url: artistURL, dataType: "jsonp", jsonpCallback: "artistResults" });
 }
 
+//call to the API to get tracks from specified artist, returns as a callback function
 function getTracks(artist) {
-    var trackURL = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_artist=" + artist + "&f_lyrics_language=en&f_has_lyrics=1&quorum_factor=1&apikey=" + key;
-    $.ajax({ url: trackURL, dataType: "jsonp", jsonpCallback: "trackResults" });
+  var trackURL = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_artist=" + artist + "&f_lyrics_language=en&f_has_lyrics=1&quorum_factor=1&apikey=" + key;
+  $.ajax({ url: trackURL, dataType: "jsonp", jsonpCallback: "trackResults" });
 }
 
+//call to the API to get lyrics from specified track, returns as a callback function
 function getLyrics(trackID) {
-    lyricID = trackID.trackID;
-    var trackURL = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=" + lyricID + "&apikey=" + key;
-    $.ajax({ url: trackURL, dataType: "jsonp", jsonpCallback: "lyricResults" });
+  lyricID = trackID.trackID;
+  var trackURL = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=" + lyricID + "&apikey=" + key;
+  $.ajax({ url: trackURL, dataType: "jsonp", jsonpCallback: "lyricResults" });
 }
 
 function generateQuestions(lyric) {
-    var titleEl = document.getElementById("question-title");
-    titleEl.textContent = lyric;
+  //get the element and display the lyric
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = lyric;
+  choicesEl.innerHTML = "";
 
-    choicesEl.innerHTML = "";
-
-    for (var i = 0; i < generatedResults.length; i++) {
-      var choice = generatedResults[i].trackName;
-      var answerButton = document.createElement("button");
-      answerButton.setAttribute("class", "choice");
-      answerButton.setAttribute("value", choice);
-
-      answerButton.textContent = i + 1 + ". " + choice;
-
-      answerButton.addEventListener("click", questionClick);
-
-      choicesEl.appendChild(answerButton);
-    }
-    console.log(answer);
+  //iterate through the results array and display in the question buttons
+  for (var i = 0; i < generatedResults.length; i++) {
+    var choice = generatedResults[i].trackName;
+    var answerButton = document.createElement("button");
+    answerButton.setAttribute("class", "choice");
+    answerButton.setAttribute("value", choice);
+    answerButton.textContent = i + 1 + ". " + choice;
+    answerButton.addEventListener("click", questionClick);
+    choicesEl.appendChild(answerButton);
+  }
+  //Our little cheat answer.  REMOVE BEFORE FINAL SUBMISSION
+  console.log(answer);
 }
 
-
+//little helper function to get a random number
 function randomize(length) {
     return Math.floor(Math.random() * (length - 1));
 }
